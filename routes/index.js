30514e4a4cfa6bf63babe9ed7362c4b66660a3f9
Jaakko Jokinen <jaakko.jokinen@gmail.com>
2016-07-29 04:50:25 +0300

    add router {strict: true}

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({strict: true});
 
 console.log('8. index.js router get');
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({strict: true});
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
 var passport = require('passport');
